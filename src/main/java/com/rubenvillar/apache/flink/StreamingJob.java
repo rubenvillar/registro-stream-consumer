3766f8e94a6a7fabc3175255f0703f228508184e
Rubén Villar Escudero <yo@rubenvillar.com>
2019-02-24 18:56:29 +0100

    Initial commit

---
+++ src/main/java/com/rubenvillar/apache/flink/StreamingJob.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.rubenvillar.apache.flink;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
+import org.apache.flink.util.Collector;
+
+import java.util.Properties;
+
+/**
+ * Skeleton for a Flink Streaming Job.
+ * <p>
+ * <p>For a tutorial how to write a Flink streaming application, check the
+ * tutorials and examples on the <a href="http://flink.apache.org/docs/stable/">Flink Website</a>.
+ * <p>
+ * <p>To package your appliation into a JAR file for execution, run
+ * 'mvn clean package' on the command line.
+ * <p>
+ * <p>If you change the name of the main class (with the public static void main(String[] args))
+ * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
+ */
+public class StreamingJob {
+
+    public static void main(String[] args) throws Exception {
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "localhost:9092");
+        properties.setProperty("group.id", "apache-flink");
+        AvroDeserializationSchema<RegistroType> avroSchema = new AvroDeserializationSchema<>(RegistroType.class);
+        FlinkKafkaConsumer011<RegistroType> kafkaConsumer = new FlinkKafkaConsumer011<>("registro-stream-partitioned", avroSchema, properties);
+        DataStream<RegistroType> stream = env.addSource(kafkaConsumer);
+
+        stream.shuffle().print().setParallelism(1);
+
+        env.execute("Registro Partitioned KafKa Consumer");
+    }
+}
